38f63b6381b51a777d44b565b7006f84e9b2c73c
KittyC513 <shuichunchenkitty@gmail.com>
2023-03-23 11:45:24 -0400

    Updated Test Result

---
+++ Assets/Scripts/TestResult.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestResult : MonoBehaviour
+{
+    public int result;
+    Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+        GenerateResult();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void GenerateResult()
+    {
+        result = Random.Range(65, 80);
+        score.text = "Your test result is   " + result;
+
+    }
+}
