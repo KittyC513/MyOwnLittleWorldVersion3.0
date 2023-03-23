d7e73c01969103789900f0bff332a73a201599dc
KittyC513 <shuichunchenkitty@gmail.com>
2023-03-23 14:44:51 -0400

    Lua Mental Health

---
+++ Assets/Scripts/MentalHealthBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MentalHealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxValue(int value)
+    {
+        slider.maxValue = value;
+        slider.value = value;
+        
+        fill.color = gradient.Evaluate(1f);
+
+    }
+
+    // Start is called before the first frame update
+
+    public void SetValue(int Mhealth)
+    {
+        slider.value = Mhealth;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+
+}
